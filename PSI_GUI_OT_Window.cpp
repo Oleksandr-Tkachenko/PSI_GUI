2eecb24b71871efafc9ce2a3ce457e978865d5bf
alex <alex@alex-Aspire>
2016-09-03 23:16:19 +0200

    Added skeleton for OT based PSI.

--- PSI_GUI_OT_Window.cpp
+++ PSI_GUI_OT_Window.cpp
@@ -13,18 +13,32 @@
 
 #include "PSI_GUI_OT_Window.h"
 
-
-
 PSI_GUI_OT_Server_Window::PSI_GUI_OT_Server_Window(QWidget* parent, QWidget * main) : PSI_GUI_SubWindowTemplate(parent, main) {
+    ui.setupUi(this);
+    this->show();
+    PSI_GUI_connectAllButtons();
+    QResource::registerResource("images.rcc");
+    ui.windowLabel->setText("PSI Oblivious Transfer Server");
 }
 
 PSI_GUI_OT_Server_Window::~PSI_GUI_OT_Server_Window() {
 }
 
-
+bool PSI_GUI_OT_Server_Window::PSI_GUI_selfCheck() {
+    return true;
+}
 
 PSI_GUI_OT_Client_Window::PSI_GUI_OT_Client_Window(QWidget* parent, QWidget * main) : PSI_GUI_SubWindowTemplate(parent, main) {
+    ui.setupUi(this);
+    this->show();
+    PSI_GUI_connectAllButtons();
+    QResource::registerResource("images.rcc");
+    ui.windowLabel->setText("PSI Oblivious Transfer Client");
 }
 
 PSI_GUI_OT_Client_Window::~PSI_GUI_OT_Client_Window() {
+}
+
+bool PSI_GUI_OT_Client_Window::PSI_GUI_selfCheck() {
+    return true;
 }
--- PSI_GUI_OT_Window.h
+++ PSI_GUI_OT_Window.h
@@ -15,15 +15,17 @@
 #define PSI_GUI_OT_WINDOW_H
 
 #include <QtGui/QWidget>
+#include <QResource>
 
 #include "PSI_GUI_SubWindowTemplate.h"
-
 
 class PSI_GUI_OT_Server_Window : public PSI_GUI_SubWindowTemplate {
     Q_OBJECT
 public:
     explicit PSI_GUI_OT_Server_Window(QWidget * parent = 0, QWidget * main = 0);
     virtual ~PSI_GUI_OT_Server_Window();
+protected:
+    bool PSI_GUI_selfCheck();
 };
 
 class PSI_GUI_OT_Client_Window : public PSI_GUI_SubWindowTemplate {
@@ -31,6 +33,8 @@ class PSI_GUI_OT_Client_Window : public PSI_GUI_SubWindowTemplate {
 public:
     explicit PSI_GUI_OT_Client_Window(QWidget * parent = 0, QWidget * main = 0);
     virtual ~PSI_GUI_OT_Client_Window();
+protected:
+    bool PSI_GUI_selfCheck();
 };
 
 #endif /* PSI_GUI_OT_WINDOW_H */
