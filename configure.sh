20b8fba9c3c0b7448f245f8f967ef6b2767d41c1
alex <alex@alex-Aspire>
2016-12-14 22:44:41 +0100

    Added bash scripts for demo

--- configure.sh
+++ configure.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-resources_dir=~/.PSI_GUI/images
-resources_file=images.qrb
+resources_dest=~/.PSI_GUI/images
+resources_file=images.qrb 
+scripts_dest=~/.PSI_GUI/
+scripts_dir=scripts/
 
 qmake -o Makefile PSI_GUI.pro
-mkdir -p $resources_dir
-cp $resources_file $resources_dir
+mkdir -p $resources_dest
+
+cp $resources_file $resources_dest 
+cp -R $scripts_dir $scripts_dest 
